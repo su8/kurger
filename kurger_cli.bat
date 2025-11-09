bc401a3b441bf4f722fd288be292fc066045e681
su8 <wifiextender2@gmail.com>
2025-11-09 19:40:05 +0300

    advice to remove quotes from given pdf file

--- kurger_cli.bat
+++ kurger_cli.bat
@@ -5,7 +5,7 @@
 COPY kurger_cli.exe C:\MingW\bin\kurger_cli.exe
 CLS
 :loop
-echo from 1 to 10 resolution 100 file 'book.pdf'
+echo from 1 to 10 resolution 100 file book.pdf  # without quotes for book.pdf
 SET /P NAME=
 C:\MingW\bin\kurger_cli.exe %name%
 PAUSE
